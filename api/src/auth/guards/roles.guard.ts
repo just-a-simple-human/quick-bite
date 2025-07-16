just-a-simple-human/e-commerce-shop

/api реализованы авторизация и сущность заказа 1. Мелкие изменения существующей аутентификации и некоторых эндпоинтов 2. Реализована авторизация с проверкой авторства при необходимости 3. Реализована сущность заказа и связанные с ней CRUD операции

api/src/auth/guards/roles.guard.ts
@@ -1,0 +1,16 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../types/permission.type';
+import { UsePermission } from '../decorators/permission.decorator';
+import { permissionChecker } from '../permission.provider';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const permission = this.reflector.get(UsePermission, context.getHandler());
+    const role: Role = context.switchToHttp().getRequest().user.role;
+    return permissionChecker.can(role, permission);
+  }
+}

api/src/auth/interseptors/check-author.interceptor.ts
@@ -1,0 +1,46 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { map, Observable } from 'rxjs';
+import { UsePermission } from '../decorators/permission.decorator';
+import { permissionChecker } from '../permission.provider';
+import { ExtractAuthor } from '../decorators/extract-author.decotator';
+
+@Injectable()
+export class CheckAuthInterceptor implements NestInterceptor {
+  constructor(private readonly reflector: Reflector) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const { action, resource } = this.reflector.get(
+      UsePermission,
+      context.getHandler(),
+    );
+    const extractAuthor = this.reflector.get(
+      ExtractAuthor,
+      context.getHandler(),
+    );
+
+    return next.handle().pipe(
+      map((value) => {
+        if (
+          permissionChecker.can(request.user.role, {
+            action,
+            resource,
+            possession: 'any',
+          }) ||
+          (Array.isArray(value)
+            ? value.every((item) => extractAuthor(item) === request.user.id)
+            : extractAuthor(value) === request.user.id)
+        ) {
+          return value;
+        }
+        throw new ForbiddenException();
+      }),
+    );
+  }
+}

api/src/customer/customer.controller.ts
@@ -6,12 +6,46 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { OrderService } from 'src/order/order.service';
+import { CheckAuthInterceptor } from 'src/auth/interseptors/check-author.interceptor';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { permissionChecker } from 'src/auth/permission.provider';
+import { Action, Resource, Role } from 'src/auth/types/permission.type';
+import { UseAuth } from 'src/auth/decorators/auth.decorator';
+import { Customer } from 'src/entities/customer.entity';
 
 @Controller('customer')
 export class CustomerController {
-  constructor(private readonly customerService: CustomerService) {}
+  constructor(
+    private readonly customerService: CustomerService,
+    private readonly orderService: OrderService,
+  ) {}
+
+  @Get(':id')
+  @UseAuth(
+    {
+      action: Action.Read,
+      resource: Resource.CustomerProfile,
+      possession: 'own',
+    },
+    (customer: Customer) => customer.id,
+  )
+  getProfileById(@Param('id') id: string) {
+    const response = this.customerService.findOneById(+id);
+    return response;
+  }
+
+  @Get(':id/order')
+  @UseGuards(JwtGuard)
+  getCustomerOrders(@Param('id') id: string) {
+    const response = this.orderService.findAllByCustomerId(+id);
+    return response;
+  }
 }

just-a-simple-human/quick-bite

api/src/auth/guards/roles.guard.ts
@@ -1,0 +1,16 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../types/permission.type';
+import { UsePermission } from '../decorators/permission.decorator';
+import { permissionProvider } from '../permission.provider';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const permission = this.reflector.get(UsePermission, context.getHandler());
+    const role: Role = context.switchToHttp().getRequest().user.role;
+    return permissionProvider.can(role, permission);
+  }
+}

api/src/auth/interseptors/check-author.interceptor.ts
@@ -1,0 +1,46 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { map, Observable } from 'rxjs';
+import { UsePermission } from '../decorators/permission.decorator';
+import { permissionProvider } from '../permission.provider';
+import { ExtractAuthor } from '../decorators/extract-author.decotator';
+
+@Injectable()
+export class CheckAuthInterceptor implements NestInterceptor {
+  constructor(private readonly reflector: Reflector) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const { action, resource } = this.reflector.get(
+      UsePermission,
+      context.getHandler(),
+    );
+    const extractAuthor = this.reflector.get(
+      ExtractAuthor,
+      context.getHandler(),
+    );
+
+    return next.handle().pipe(
+      map((value) => {
+        if (
+          permissionProvider.can(request.user.role, {
+            action,
+            resource,
+            possession: 'any',
+          }) ||
+          (Array.isArray(value)
+            ? value.every((item) => extractAuthor(item) === request.user.id)
+            : extractAuthor(value) === request.user.id)
+        ) {
+          return value;
+        }
+        throw new ForbiddenException();
+      }),
+    );
+  }
+}

api/src/customer/customer.controller.ts
@@ -6,12 +6,46 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { OrderService } from 'src/order/order.service';
+import { CheckAuthInterceptor } from 'src/auth/interseptors/check-author.interceptor';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { permissionProvider } from 'src/auth/permission.provider';
+import { Action, Resource, Role } from 'src/auth/types/permission.type';
+import { UseAuth } from 'src/auth/decorators/auth.decorator';
+import { Customer } from 'src/entities/customer.entity';
 
 @Controller('customer')
 export class CustomerController {
-  constructor(private readonly customerService: CustomerService) {}
+  constructor(
+    private readonly customerService: CustomerService,
+    private readonly orderService: OrderService,
+  ) {}
+
+  @Get(':id')
+  @UseAuth(
+    {
+      action: Action.Read,
+      resource: Resource.CustomerProfile,
+      possession: 'own',
+    },
+    (customer: Customer) => customer.id,
+  )
+  getProfileById(@Param('id') id: string) {
+    const response = this.customerService.findOneById(+id);
+    return response;
+  }
+
+  @Get(':id/order')
+  @UseGuards(JwtGuard)
+  getCustomerOrders(@Param('id') id: string) {
+    const response = this.orderService.findAllByCustomerId(+id);
+    return response;
+  }
 }